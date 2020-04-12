6b221d48a0298ce121f5c52d40528692c1caf04c
kosa3 <suga.tech3@gmail.com>
2020-04-12 21:02:33 +0900

    refactoring

--- subscriber/main.go
+++ subscriber/main.go
@@ -6,26 +6,7 @@ import (
 	"github.com/olivere/elastic/v7"
 	"github.com/streadway/amqp"
 	"log"
-	"time"
 )
-
-type SensorData struct {
-	DeviceId string  `json:"id"`
-	Temp     float64 `json:"temp"`
-	Lux      float64 `json:"lux"`
-	Humidity float64 `json:"humidity"`
-}
-
-type Protocol struct {
-	SensorData
-	Timestamp time.Time
-}
-
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-	}
-}
 
 func main() {
 	// rabbitmqからキューを受け取りelasticsearchにデータを投入するsub
